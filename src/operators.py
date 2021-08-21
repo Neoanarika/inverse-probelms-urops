d1473eba88708663695b0f50c8a29336f8f72fdc
neoanarika <angmingliang4017ic@gmail.com>
2021-08-21 15:58:52 +0800

    add new random occulude

--- src/operators.py
+++ src/operators.py
@@ -27,3 +27,32 @@ class CenterOcclude(nn.Module):
         else:
             A = F.pad(A, (pad_size_x, pad_size_x, pad_size_y, pad_size_y), "constant", 1)
         return A
+
+def get_coord(sample, imgshape):
+  y = sample %imgshape[2]
+  x = (sample-y)//imgshape[2]
+  return x,y 
+
+class RandomOcclude(nn.Module):
+
+    def __init__(self, config):
+        super(CenterOcclude, self).__init__()
+
+        num = config["operator_params"]["num_measurements"] 
+        batch_size = config["exp_params"]["batch_size"]
+        image_shape = config["exp_params"]["image_shape"]
+        self.A = self.occlude(num, [batch_size] + image_shape)
+    
+    def forward(self, x):
+        return torch.mul(self.A, x)
+    
+    @staticmethod
+    def occlude(num, image_shape):
+        b, c, x, y = image_shape
+        n = x * y
+        ri = np.random.choice(n, num, replace=False) # random sample of indices
+        A = torch.zeros(image_shape)
+        for sample in ri:
+            x, y = get_coord(sample, image_shape)
+            A[:, :, x, y] = 1
+        return A
