7e7d723b448f3f21000260573b8b4255292f02dd
neoanarika <angmingliang4017ic@gmail.com>
2021-09-30 21:27:28 +0800

    created dataset for optimisation

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -1,4 +1,4 @@
-from utils.download_mnist import mnist_dataloader_train
+from utils.download_mnist import mnist_dataloader_train, modified_mnist_dataloader_train
 from utils.download_celeba import celeba_dataloader 
 
 def dataloader(config):
@@ -10,4 +10,8 @@ def dataloader(config):
                             config["exp_params"]["data_path"])
     elif config["exp_params"]["dataset"] == "mnist":
         dm = mnist_dataloader_train(config)
+    elif config["exp_params"]["dataset"] == "modified_mnist":
+        dm = modified_mnist_dataloader_train(config)
+    else:
+        raise Exception("Dataset handler not defined")
     return dm 
--- utils/download_mnist.py
+++ utils/download_mnist.py
@@ -1,3 +1,4 @@
+import torch
 from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 from torchvision import transforms
@@ -22,6 +23,28 @@ def mnist_dataloader_test(config, path=""):
     mnist_test = MNIST(f'{path}/{config["exp_params"]["data_path"]}', train=False, download=False,
                          transform=transforms.Compose([transforms.ToTensor(),
                                                        transforms.Resize((32,32))]))
+    dm = DataLoader(mnist_test, batch_size=config["exp_params"]["batch_size"])
+    return dm
+
+def add_gradient(x):
+    y = torch.linspace(0, 1, steps=32)
+    filter_mat = y.repeat(32,1).t()
+    return x + filter_mat
+
+def modified_mnist_dataloader_train(config):
+    mnist_train = MNIST(f'{config["exp_params"]["data_path"]}', train=True, download=False,
+                         transform=transforms.Compose([transforms.ToTensor(),
+                                                       transforms.Resize((32,32)),
+                                                       lambda x: add_gradient(x)
+                                                       ]))
+    dm = DataLoader(mnist_train, batch_size=config["exp_params"]["batch_size"])
+    return dm
+
+def modified_mnist_dataloader_test(config, path=""):
+    mnist_test = MNIST(f'{path}/{config["exp_params"]["data_path"]}', train=False, download=False,
+                         transform=transforms.Compose([transforms.ToTensor(),
+                                                       transforms.Resize((32,32)),
+                                                       lambda x: add_gradient(x)]))
     dm = DataLoader(mnist_test, batch_size=config["exp_params"]["batch_size"])
     return dm
 
