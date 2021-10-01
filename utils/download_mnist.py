e0fe574d34924210fb5e1a8193c51c436e4639a5
neoanarika <angmingliang4017ic@gmail.com>
2021-10-01 12:18:05 +0800

    updaed modify mnist

--- utils/download_mnist.py
+++ utils/download_mnist.py
@@ -12,27 +12,30 @@ def download_mnist(config):
                          transform=transforms.Compose([transforms.ToTensor(),
                                                        transforms.Resize((32,32))]))
 
-def mnist_dataloader_train(config):
-    mnist_train = MNIST(f'{config["exp_params"]["data_path"]}', train=True, download=False,
+def mnist_dataloader_train(config, path=""):
+    path = "" if path == "" else path+"/"
+    mnist_train = MNIST(f'{path}{config["exp_params"]["data_path"]}', train=True, download=False,
                          transform=transforms.Compose([transforms.ToTensor(),
                                                        transforms.Resize((32,32))]))
     dm = DataLoader(mnist_train, batch_size=config["exp_params"]["batch_size"])
     return dm
 
 def mnist_dataloader_test(config, path=""):
-    mnist_test = MNIST(f'{path}/{config["exp_params"]["data_path"]}', train=False, download=False,
+    path = "" if path == "" else path+"/"
+    mnist_test = MNIST(f'{path}{config["exp_params"]["data_path"]}', train=False, download=False,
                          transform=transforms.Compose([transforms.ToTensor(),
                                                        transforms.Resize((32,32))]))
     dm = DataLoader(mnist_test, batch_size=config["exp_params"]["batch_size"])
     return dm
 
 def add_gradient(x):
-    y = torch.linspace(0, 1, steps=32)
+    y = torch.linspace(0, 0.5, steps=32)
     filter_mat = y.repeat(32,1).t()
-    return x + filter_mat
+    return 0.5*x + filter_mat
 
-def modified_mnist_dataloader_train(config):
-    mnist_train = MNIST(f'{config["exp_params"]["data_path"]}', train=True, download=False,
+def modified_mnist_dataloader_train(config, path=""):
+    path = "" if path == "" else path+"/"
+    mnist_train = MNIST(f'{path}{config["exp_params"]["data_path"]}', train=True, download=False,
                          transform=transforms.Compose([transforms.ToTensor(),
                                                        transforms.Resize((32,32)),
                                                        lambda x: add_gradient(x)
@@ -41,7 +44,8 @@ def modified_mnist_dataloader_train(config):
     return dm
 
 def modified_mnist_dataloader_test(config, path=""):
-    mnist_test = MNIST(f'{path}/{config["exp_params"]["data_path"]}', train=False, download=False,
+    path = "" if path == "" else path+"/"
+    mnist_test = MNIST(f'{path}{config["exp_params"]["data_path"]}', train=False, download=False,
                          transform=transforms.Compose([transforms.ToTensor(),
                                                        transforms.Resize((32,32)),
                                                        lambda x: add_gradient(x)]))
